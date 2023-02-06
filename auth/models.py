5ca906c4662c5768a23b5b61e7abe136c8cec1b0
jatin218tec <90670002+jatin218tec@users.noreply.github.com>
2023-02-06 20:27:28 +0000

    Pending changes exported from your codespace

---
+++ auth/models.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    email = models.EmailField(primary_key=True, unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
